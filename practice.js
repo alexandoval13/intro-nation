0f49868cd7ed5b4d2a5f53a355836e94d8c3e766
Alexandra Sandoval <alexandoval13@gmail.com>
2020-10-02 22:04:58 -0700

    Write multiplier

--- practice.js
+++ practice.js
@@ -10,3 +10,10 @@ addTwoValues(123, 768);
 function subtractTwoValues(input1, input2) {
   return input1 - input2;
 }
+
+
+// Create calculator function
+
+function multiply(input1, input2) {
+  return input1 * input2;
+}
