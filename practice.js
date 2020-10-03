8b09e40b4131188555a7f75b0082891de3ba7a17
Alexandra Sandoval <alexandoval13@gmail.com>
2020-10-02 22:05:43 -0700

    Write divider

--- practice.js
+++ practice.js
@@ -3,17 +3,21 @@
 
 function addTwoValues (input1, input2) {
   return input1 + input2;
-}
+};
 
 addTwoValues(123, 768);
 
 function subtractTwoValues(input1, input2) {
   return input1 - input2;
-}
+};
 
 
 // Create calculator function
 
 function multiply(input1, input2) {
   return input1 * input2;
+};
+
+function divide(input1, input2) {
+  return input1/input2;
 }
