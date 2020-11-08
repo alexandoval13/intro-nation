02bd2a1fef9c26bfc90610bc4e364ba46bc91c99
Alexandra Sandoval <alexandoval13@gmail.com>
2020-11-07 21:33:20 -0800

    Implement reverseInteger

--- intejerReverse.js
+++ intejerReverse.js
@@ -12,6 +12,16 @@
 
 
 function reverseInteger(number){
+  var reversed = arguments[1] || 0;
 
+  var lastNum = number % 10;
+
+  reversed = reversed * 10 + lastNum;
+
+  if (((number - lastNum) / 10) < 1) {
+    return reversed;
+  }
+
+  return reverseInteger(((number - lastNum) / 10), reversed);
 
 }
