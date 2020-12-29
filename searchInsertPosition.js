916b166b4d626983d203fc88b053681c85e3fd1d
Alexandra Sandoval <alexandoval13@gmail.com>
2020-12-29 00:23:07 -0800

    searchInsertPosition solution

---
+++ searchInsertPosition.js
@@ -0,0 +1,10 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+var searchInsert = function(nums, target) {
+  var position = null;
+  for (var i = 0; nums[i] <= target; i++) {
+    if (nums[i] === target) { return i };
+    position = i;
+  }
+  return position;
+};
