95c5e279f9d6ed02e033deb13a1dc17b7ee0b5aa
Alexandra Sandoval <alexandoval13@gmail.com>
2021-01-14 14:24:33 -0800

    Refactor helper function based on test results

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -6,10 +6,11 @@ You may assume the two numbers do not contain any leading zero, except the numbe
 */
 
 var listToNumber = function(list) {
+  debugger;
   // declare number
   var numberStr = ''
   // iterate through list in reverse
-  for (var i = list.length - 1; i > 0; i--) {
+  for (var i = list.length - 1; i >= 0; i--) {
     numberStr += String(list[i]);
   }
   // return number
@@ -29,10 +30,10 @@ var addTwoNumbers = function(l1, l2) {
 // TEST SUITE
 var assertEquals = function(expected, returned, name) {
   if (expected === returned) {
-    console.log('Test "', name + '" returned expected results');
+    console.log('Test \"', name + '" returned expected results.');
   } else {
-    console.log('Test "', name + '" did not return expected results. Received [' + returned + '], expected [' + expected + '].');
+    console.log('Test \"', name + '" did not return expected results. Received [' + returned + '], expected [' + expected + '].');
   }
 }
 // helper func
-assertEquals(321, listToNumber(1, 2, 3), 'Function listToNumber should return a reversed number from an arrray of numbers');
+assertEquals(321, listToNumber([1, 2, 3]), 'Function listToNumber should return a reversed number from an arrray of numbers');
