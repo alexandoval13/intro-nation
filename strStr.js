15d2064034cf4fd4d0003694db057331c3aad863
Alexandra Sandoval <alexandoval13@gmail.com>
2021-01-15 14:57:24 -0800

    Pseudocode solution to stStr without the use of indexOf

--- strStr.js
+++ strStr.js
@@ -8,6 +8,8 @@ What should we return when needle is an empty string? This is a great question t
 For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
 */
 
+// simple solution
+
 var strStr = function(haystack, needle) {
   //edge case
   if (needle.length === 0) {
@@ -15,4 +17,22 @@ var strStr = function(haystack, needle) {
   }
 
   return haystack.indexOf(needle);
+};
+
+// consider solution to problem without using indexOf
+
+var strStr = function(haystack, needle) {
+  //edge case
+  if (needle.length === 0) {
+    return 0;
+  }
+
+  // iterate through haystack
+    // if char matches first char of needle
+      // iterate through subsequent chars in needle
+        // check each subsequent needle character for a match against subsequent haystack chars
+        // if not a match, end iteration
+        // if the end of needle is reached, return the index original matched char in haystack
+
+  // return -1 for no match
 };
