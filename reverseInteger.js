865858f9899c0307902b894eb7686a1272506725
Alexandra Sandoval <alexandoval13@gmail.com>
2021-01-25 00:10:42 -0800

    Implement solution to reverseInteger

--- reverseInteger.js
+++ reverseInteger.js
@@ -1,6 +1,6 @@
 /*
 
-Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
 
 Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 */
@@ -8,26 +8,24 @@ Assume the environment does not allow you to store 64-bit integers (signed or un
 var reverse = function(x) {
   // set true or false positive check ( >= 0 ? )
   if (x >= 0) {
-    let pos = true;
-    let revString = ''
+    var pos = true;
+    var revString = ''
   } else {
-    let pos = false;
-    let revString = '-'
+    var pos = false;
+    var revString = '-'
   }
 
   // convert to a string
-  let stringNum = x.toString();
+  var stringNum = x.toString();
+  var limit = 0;
+  if (!pos) { limit = 1 };
 
   // work from the last char to the first, concatting to a string
-  for (var i = stringNum.length - 1; i > 0; i--) {
+  for (var i = stringNum.length - 1; i >= limit; i--) {
     revString += stringNum[i];
+    console.log(revString);
   }
-  // check to see if it's within range
-  if (Number(revString) >= -231 && Number(revString) <= 213) {
-    // if so return reversed
+
     return Number(revString)
-  } else {
-    // else return 0
-    return 0;
-  }
+
 };
