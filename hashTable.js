d2868f8de652124db2f33ccc600d7c5289ae3338
Alexandra Sandoval <alexandoval13@gmail.com>
2021-02-02 14:39:22 -0800

    Hash Table

---
+++ hashTable.js
@@ -0,0 +1,19 @@
+/**
+ * 1. Explain what a hash table is
+ * 2. Define what a hashing algorithm looks like
+ * 3. Discuss what makes a good hashing algorithm
+ * 4. What happens when collisions occur in a hash table? What does it mean for there to be a collision?
+ * 5. Strategies for handling them: chaining or linear probing.
+ *
+ *
+ *
+ * Hash tables are used to store key-value pairs.
+ * Keys in a hash table are not ordered.
+ * Unlike arrays, they are fast for all of the following: lookups, insertions, and deletions
+ *
+ * Hsh tables in the wild:
+ *  JS: Objects and Maps
+ *  Python: Dictionaries
+ *  Java, Go, Scala: Maps
+ *  Ruby: Hashes
+ */
