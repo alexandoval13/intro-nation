1f4b874e018f5b95b37d6bd0d2f83d27e1f0d7bc
Alexandra Sandoval <alexandoval13@gmail.com>
2021-02-06 17:43:47 -0800

    Implement countUniqueValues

---
+++ AlgorithmsAndDataStructures/countUniqueValues.js
@@ -0,0 +1,45 @@
+const countUniqueValues = (arr) => {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  let a = 0;
+  let b = 1;
+
+  while (b < arr.length) {
+    if (arr[a] === arr[b]) {
+      b++;
+    } else {
+      a++;
+      arr[a] = arr[b];
+      b++;
+    }
+  }
+
+  return a + 1;
+};
+
+console.log(
+  'function returns 0 for an empty array:',
+  countUniqueValues([]) === 0
+);
+console.log(
+  'function returns number of unique values:',
+  countUniqueValues([1]) === 1
+);
+console.log(
+  'function returns number of unique values:',
+  countUniqueValues([1, 2, 3]) === 3
+);
+console.log(
+  'function returns number of unique values:',
+  countUniqueValues([1, 1, 2]) === 2
+);
+console.log(
+  'function returns number of unique values:',
+  countUniqueValues([1, 1, 2, 2, 2, 3, 4, 5]) === 5
+);
+console.log(
+  'function returns number of unique values:',
+  countUniqueValues([1, 1, 2, 3, 4, 5, 5, 5, 6, 6, 7]) === 7
+);
