7c15206675ab2b51c1f2317d77c1ce5f37a0e48a
Alexandra Sandoval <alexandoval13@gmail.com>
2021-02-15 18:17:46 -0800

    v2

--- PracticeProbs/houseRobber.js
+++ PracticeProbs/houseRobber.js
@@ -51,17 +51,41 @@ var rob = function (nums) {
   return max;
 };
 
-// [1, 2, 3, 4]
-//  x           h=1, v=1,  max = 1
-//  -     x     h=3, v=1+3,  max = 4
-//  -     -     ?
-//  -     -       ?
-//  -        x  h=4, v=1+4, max = 5
+// v2
+// var rob = function (nums) { //  [1, 2, 3, 4]
+//   if(nums.length === 0) {
+//       return 0;
+//   }
 
-//     x        h=2, v=2, max = 5
-//     -     x  h=4, v=2+4, max = 6
-//     -     -    ?
-//     -     -      ?
+// let maxX = nums[0] || 0; // 1
+// let maxY = nums[1] || 0; // 2
+
+// for (var i = 0; i < nums.length; i+=3) { // i = 0
+// console.log('LOOPING: value of i is ', i )
+//    let first = i; // 0
+//    let second = i + 1; // 1
+//    let third = second+1; // 2
+//    let fourth = third+1; // 3
+//    let fifth = fourth+1; // 4
+
+//    let x1 = maxX + (nums[fourth] || 0); // 1 + 4 = 5
+//    let x2 = maxY + (nums[fourth] || 0); // 2 + 4 = 6
+//    console.log('Xs:', x1, x2);
+//    console.log(maxX);
+
+//    let y1 = maxX + (nums[third] || 0) + (nums[fifth] || 0); // 6 + 0 + 0 = 6
+//    let y2 = maxY + (nums[fifth]||0); // 4 + 0 = 4
+//    console.log('Ys:', y1, y2); // 4
+//    console.log(maxY);
+
+//    maxX = Math.max(x1, x2); // 6
+//    maxY = Math.max(y1, y2);
+// }
+
+// return Math.max(maxX, maxY);
+// };
+
+// let test2 = rob([6,3,10,8,2,10,3,5,10,5,3]) // <--- DOESNT WORK WITH v2
 
 let test = rob([1, 2, 3, 4]);
 console.log(test, test === 6);
@@ -92,7 +116,3 @@ let test2 = rob([
   124,
 ]);
 console.log(test2);
-
-// let test = rob([1, 2, 3, 4, 5]);
-// let test = rob([1, 2, 3, 4, 5]);
-// let test = rob([1, 2, 3, 4, 5]);
