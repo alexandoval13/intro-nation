f686c65114b996dbf56cb442bf3f9eede6a9f3e6
Alexandra Sandoval <alexandoval13@gmail.com>
2021-02-20 09:52:22 -0800

    validAnagrams and frequency counter

--- AlgorithmsAndDataStructures/AlgorithmPatterns/validAnagram.js
+++ AlgorithmsAndDataStructures/AlgorithmPatterns/validAnagram.js
@@ -1,49 +1,101 @@
 // solve using the frequency counter pattern
+// const validAnagram = (string1, string2) => {
+//   if (string1.length !== string2.length) {
+//     return false;
+//   }
+//   let letterObj = {};
+//   for (var i = 0; i < string1.length; i++) {
+//     var letter = string1[i];
+//     if (!letterObj[letter]) {
+//       letterObj[letter] = 0;
+//     }
+//     letterObj[letter]++;
+//   }
+//   for (var j = 0; j < string2.length; j++) {
+//     var letter = string2[j];
+//     if (!letterObj[letter]) {
+//       return false;
+//     }
+//     letterObj[letter]--;
+//   }
+//   return true;
+// };
+
+// solution 2/20/21
 const validAnagram = (string1, string2) => {
+  // check lengths
   if (string1.length !== string2.length) {
+    // return false if not equal
     return false;
   }
-
-  let letterObj = {};
-  for (var i = 0; i < string1.length; i++) {
-    var letter = string1[i];
-    if (!letterObj[letter]) {
-      letterObj[letter] = 0;
-    }
-    letterObj[letter]++;
+  // intialize tracker
+  let chars = {};
+  // iterate over string 1
+  for (var char of string1) {
+    // add char to a count tracker
+    chars[char] ? chars[char]++ : (chars[char] = 1);
   }
-
-  for (var j = 0; j < string2.length; j++) {
-    var letter = string2[j];
-    if (!letterObj[letter]) {
+  console.log(chars);
+  // iterate over string 2
+  for (var char of string2) {
+    // if char does not exist in tracker or if tracker count is less than zero
+    if (!chars[char]) {
+      // return false
       return false;
+      // else
+    } else {
+      // decrement char count from tracker
+      chars[char]--;
     }
-    letterObj[letter]--;
   }
-
+  // return bool that represents if inputs are anagrams
   return true;
 };
 
 // tests
+const assertEquals = (actual, expected, testName) => {
+  if (actual === expected) {
+    console.log('PASSED');
+  } else {
+    console.log(
+      `FAILED ${testName} -- expected ${expected} but returned ${actual}`
+    );
+  }
+};
 
-console.log(
-  'validAnagrams returns false if strings are different lengths:',
-  validAnagram('a', 'ab') === false
+assertEquals(
+  validAnagram('', ''),
+  true,
+  'returns true for empty string inputs'
 );
-console.log(
-  'validAnagrams returns false if letters do not match:',
-  validAnagram('aaa', 'zzz') === false
-);
-console.log(
-  'validAnagrams returns false if letter counts do not match:',
-  validAnagram('aazz', 'azzz') === false
-);
-console.log(
-  'validAnagrams returns true if letters and letter counts match:',
-  validAnagram('azam', 'maza') === true
+assertEquals(
+  validAnagram('a', 'ab'),
+  false,
+  'returns false if strings are different lengths'
 );
 
-console.log(validAnagram('anagrams', 'nagarams'));
-console.log(validAnagram('texty', 'ytext'));
-console.log(validAnagram('awesome', 'awesom'));
-console.log(validAnagram('rat', 'ran'));
+assertEquals(
+  validAnagram('a', 'z'),
+  false,
+  'returns false if letters do not match'
+);
+assertEquals(
+  validAnagram('zz', 'zzz'),
+  false,
+  'returns false if letter counts do not match'
+);
+assertEquals(
+  validAnagram('azam', 'maza'),
+  true,
+  'returns true if letters and letter counts match'
+);
+assertEquals(
+  validAnagram('texttwisttime', 'timetwisttext'),
+  true,
+  'returns true if letters and letter counts match'
+);
+assertEquals(
+  validAnagram('awesome', 'awesom'),
+  false,
+  "returns false if counts don't match"
+);
