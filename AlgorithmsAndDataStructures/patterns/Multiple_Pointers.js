5a8077b23d81dc5d726d28beb2e8eda7a384664c
Alexandra Sandoval <alexandoval13@gmail.com>
2021-02-20 14:48:10 -0800

    Implement isSubsequence andaveragePair

--- AlgorithmsAndDataStructures/patterns/Multiple_Pointers.js
+++ AlgorithmsAndDataStructures/patterns/Multiple_Pointers.js
@@ -7,6 +7,15 @@ const assertEquals = (actual, expected, testName) => {
     );
   }
 };
+const assertArrayEquals = (actual, expected, testName) => {
+  if (JSON.stringify(actual) === JSON.stringify(expected)) {
+    console.log('PASSED');
+  } else {
+    console.log(
+      `FAILED ${testName} -- expected ${expected} but returned ${actual}`
+    );
+  }
+};
 
 // Implement solution using a multiple pointers pattern
 const sumZero = (integers) => {
@@ -129,3 +138,105 @@ function areThereDuplicates(...args) {
   }
   return false;
 }
+
+function averagePair(integers, target) {
+  if (integers.length === 0) {
+    return false;
+  }
+  // set a pointer at beginning, a pointer at the end
+  let p1 = 0;
+  let p2 = integers.length - 1;
+  // as long as they dont equal each other
+  while (p1 !== p2) {
+    let average = (integers[p1] + integers[p2]) / 2;
+    // if the average of the values is equal to the input val
+    if (average === target) {
+      // return the pair
+      return true;
+      // if it's greater
+    } else if (average > target) {
+      // decrement pointer 2
+      p2--;
+    } else {
+      // if it's less than
+      // increment p1
+      p1++;
+    }
+  }
+  // return []
+  return false;
+}
+
+// TESTS
+// assertEquals(
+//   averagePair([], 43),
+//   false,
+//   'returns false for empty array'
+// );
+// assertEquals(averagePair([1, 1], 1), true, 'returns true when pair exists');
+// assertEquals(
+//   averagePair([1, 2], 1),
+//   false,
+//   'returns false when pair does not exist'
+// );
+// assertEquals(
+//   averagePair([1, 2, 3, 4, 5], 2),
+//   true,
+//   'returns true when pair does exist'
+// );
+
+function isSubsequence(string1, string2) {
+  if (string1.length > string2.length || string1.length === 0) {
+    return false;
+  }
+  let p1 = 0;
+  let p2 = 0;
+  for (var i = 0; i < string2.length; i++) {
+    if (string1[p1] === string2[p2]) {
+      p1++;
+      if (p1 === string1.length) {
+        return true;
+      }
+    }
+    p2++;
+  }
+  return false;
+}
+
+// TESTS
+// assertEquals(isSubsequence('', ''), false, 'returns false for empty strings');
+// assertEquals(
+//   isSubsequence('str', ''),
+//   false,
+//   'returns false for empty strings'
+// );
+// assertEquals(
+//   isSubsequence('', 'str'),
+//   false,
+//   'returns false for empty strings'
+// );
+// assertEquals(
+//   isSubsequence('str', 'str'),
+//   true,
+//   'returns true for the same strings'
+// );
+// assertEquals(
+//   isSubsequence('strr', 'str'),
+//   false,
+//   'returns false if string1 is greater'
+// );
+// assertEquals(
+//   isSubsequence('str', 'string'),
+//   true,
+//   'returns true if string1 is a subset of string2'
+// );
+// assertEquals(
+//   isSubsequence('sing', 'string'),
+//   true,
+//   'returns true if string1 is a subset of string2'
+// );
+// assertEquals(
+//   isSubsequence('abc', 'acb'),
+//   false,
+//   'returns false if string2 has the same characters but in a different order'
+// );
