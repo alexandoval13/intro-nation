fbfffa58b28fc227f8f2caa8edac45c5dec69f87
Alexandra Sandoval <alexandoval13@gmail.com>
2021-02-24 13:07:02 -0800

    more insertion

--- AlgorithmsAndDataStructures/sort/insertion.js
+++ AlgorithmsAndDataStructures/sort/insertion.js
@@ -2,7 +2,8 @@
 Insertion Sort
 - similar to bubble and selection
 
-- situations where insertion sort does well
+Situations where insertion sort does well
+- If your data is sorting as some data comes along, inserting at a moment's notice
 
 How it works:
 - builds up the sort by gradually creating a larger left pportion which is always sorted
@@ -11,6 +12,9 @@ How it works:
 - compare it to the the one before it and swap if necessary
 - continue to the next element and if it is in the incorrect order, iterate through the sorted portion to place the element in the correct place
 - repeat until the array is sorted
+
+
+BigO: O(n^2)
 
 Link: https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/learn/lecture/8344158#notes
 */
@@ -38,4 +42,29 @@ function insertionSort(array) {
   return array;
 }
 
-console.log(insertionSort([3, 4, 2, 5, 0]));
+// console.log(insertionSort([3, 4, 2, 5, 0]));
+
+// UDEMY COURSE ALTERNATE SOLUTION
+function insertionSortUdemy(arr) {
+  for (var i = 1; i < arr.length; i++) {
+    var currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
+
+// console.log(insertionSortUdemy([3, 4, 2, 5, 0]));
+
+function insertionSortHybrid(array) {
+  for (var i = 1; i < array.length; i++) {
+    for (var j = i; j > 0 && array[j] < array[j - 1]; j--) {
+      swap(array, j, j - 1);
+    }
+  }
+  return array;
+}
+
+console.log(insertionSortHybrid([3, 4, 2, 5, 0]));
